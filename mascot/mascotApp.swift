99c4428275bab14e7333f089c5ae2e8f4d403692
jose julian Lopez Huacuja <jose.lopez4636@alumnos.udg.mx>
2025-05-07 11:39:18 -0600

    Initial Commit

---
+++ mascot/mascotApp.swift
@@ -0,0 +1,17 @@
+//
+//  mascotApp.swift
+//  mascot
+//
+//  Created by Jose julian Lopez on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct mascotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
